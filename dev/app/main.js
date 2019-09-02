c0c1cdf77f9f94ca43f48a982c4a59c48145be41
shkredovdmitriy <shkredovdmitriy@gmail.com>
2019-09-02 23:35:07 +0300

    Создал основу для мобильного меню

--- dev/app/main.js
+++ dev/app/main.js
@@ -5,3 +5,5 @@ import '../common/scss/main.scss';
 import '../components/ds-modal/ds-modal';
 import '../components/ds-select/ds-select';
 import '../components/ds-accordion/ds-accordion';
+
+import '../components/ds-mob-menu/ds-mob-menu';
